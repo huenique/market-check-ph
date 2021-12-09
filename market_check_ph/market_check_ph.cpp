49de022008e85f46d758750e200d2223084aab7c
Hju Kneyck Flores <hjucode@gmail.com>
2021-12-09 19:55:20 +0800

    Add public api for pse rest endpoints

--- market_check_ph/market_check_ph.cpp
+++ market_check_ph/market_check_ph.cpp
@@ -5,6 +5,7 @@
 
 int main(int argc, char** argv) {
     PseData api;
-    std::string res = api.fetchStock("JFC");
+    // std::string res = api.fetchStock("JFC");
+    std::string res = api.fetchMarket("", "03-15-2017", "12-01-2019");
     std::cout << res;
 }
--- market_check_ph/pse.h
+++ market_check_ph/pse.h
@@ -4,23 +4,71 @@
 
 class PseData {
 public:
+	const std::string c_format_json = ".json";
 	const cpr::Url c_pse_endpoint = cpr::Url{ "http://pseapi.com/api/" };
 	const cpr::Url c_psei_endpoint = cpr::Url{ "http://phisix-api.appspot.com/stocks" };
 
+	/* Perform get request */
+	std::string get(cpr::Url urlPath) {
+		cpr::Response resp = cpr::Get(urlPath);
+		return resp.text;
+	}
+
+	/* Parse chucks of data. */
+	std::string fetchBigData(
+		std::string date = "",
+		std::string from = "",
+		std::string to = "",
+		std::string dir = "")
+	{
+		cpr::Url endpoint = c_pse_endpoint + cpr::Url(dir);
+		cpr::Url urlPath;
+		if (!date.empty() && (from.empty() || to.empty())) {
+			urlPath = endpoint + cpr::Url(date);
+		}
+		else if (date.empty() && (!from.empty() && !to.empty()))
+		{
+			urlPath = endpoint + cpr::Url(from + "/" + to);
+		}
+		return get(urlPath);
+	}
+
+	/* Get stock quote for the specified stock.*/
 	std::string fetchStock(std::string stockSym, std::string date = "")
 	{
-		cpr::Url endpoint = c_psei_endpoint + cpr::Url{ "/" };
 		cpr::Url urlPath;
 		if (!date.empty())
 		{
-			urlPath = endpoint + cpr::Url(stockSym) + cpr::Url(".");
-			urlPath = urlPath + cpr::Url(date) + cpr::Url(".json");
+			urlPath = c_psei_endpoint + cpr::Url("/" + stockSym + "." + date + c_format_json);
 		}
 		else
 		{
-			urlPath = endpoint + cpr::Url(stockSym) + cpr::Url(".json");
+			urlPath = c_psei_endpoint + cpr::Url("/" + stockSym + c_format_json);
 		}
-		cpr::Response resp = cpr::Get(urlPath);
-		return resp.text;
+		return get(urlPath);
+	}
+
+	/* Get all stock quote. */
+	std::string fetchAll()
+	{
+		return get(c_psei_endpoint + c_format_json);
+	}
+
+	/* Get market end of day report. */
+	std::string fetchMarket(
+		std::string date = "",
+		std::string from = "",
+		std::string to = "")
+	{
+		return fetchBigData(date, from, to, "Market/");
+	}
+
+	/* Get sectoral summary. */
+	std::string fetchSector(
+		std::string date = "",
+		std::string from = "",
+		std::string to = "")
+	{
+		return fetchBigData(date, from, to, "Sector/");
 	}
 };
